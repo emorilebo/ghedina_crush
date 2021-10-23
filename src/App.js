c773dc22c8afa86a49a1e9f9ea1d74af68a1e242
emorilebo <emorylebo@gmail.com>
2021-10-23 01:52:22 +0100

    Colors of threes replaced with empty strings

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
-  const randomColorArrangement = [];
+
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (square) => (currentColorArrangement[square] = "")
+        );
+      }
+    }
+  };
+
   const createBoard = () => {
+    const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
       const randomNumberFrom0to5 = Math.floor(
         Math.random() * candyColors.length
@@ -20,13 +38,25 @@ const App = () => {
   useEffect(() => {
     createBoard();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+  }, [checkForColumnOfThree, currentColorArrangement]);
   console.log(currentColorArrangement);
 
   return (
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((candyColor, index) => (
-          <img key={index} style={{ backgroundColor: candyColor }} />
+          <img
+            key={index}
+            style={{ backgroundColor: candyColor }}
+            alt={candyColor}
+          />
         ))}
       </div>
     </div>
