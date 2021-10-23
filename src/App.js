552d199f6c480e92e5627714c985a81938f7c397
emorilebo <emorylebo@gmail.com>
2021-10-23 01:11:40 +0100

    Generating an array of 64 colors at random

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ const width = 8;
 const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
 const App = () => {
+  const randomColorArrangement = [];
   const createBoard = () => {
     for (let i = 0; i < width * width; i++) {
-      const randomColor =
-        candyColors[Math.floor(Math.random() * candyColors.length)];
+      const randomNumberFrom0to5 = Math.floor(
+        Math.random() * candyColors.length
+      );
+      const randomColor = candyColors[randomNumberFrom0to5];
+      randomColorArrangement.push(randomColor);
     }
+    console.log(randomColorArrangement);
   };
+  createBoard();
 
   return <div>Hello</div>;
 };
